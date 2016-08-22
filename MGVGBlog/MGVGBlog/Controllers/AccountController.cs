53429bae1167bacf03bc21e05659d57c727afef2
Vladimir Gadzhov <vladko_gadjov@abv.bg>
2016-08-22 16:59:44 +0300

    fixed login
    login field requred email form

--- MGVGBlog/MGVGBlog/Controllers/AccountController.cs
+++ MGVGBlog/MGVGBlog/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace MGVGBlog.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -86,7 +86,7 @@ namespace MGVGBlog.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Wrong username or password.");
                     return View(model);
             }
         }
