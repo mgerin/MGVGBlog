cad858189ec8202f46ef20d6be7191921b1cb7cb
Vladimir Gadzhov <vladko_gadjov@abv.bg>
2016-09-01 09:32:35 +0300

    update

--- MGVGBlog/MGVGBlog/Controllers/PostsController.cs
+++ MGVGBlog/MGVGBlog/Controllers/PostsController.cs
@@ -16,11 +16,21 @@ namespace MGVGBlog.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Posts
-        public ActionResult Index()
+        public ActionResult Index(string search)
         {
             List<Post> posts = db.Posts.Include(p => p.Author).OrderBy(p => p.Id).ToList();
-            ViewBag.posts = posts;
-            return View(db.Posts.ToList());
+            var result = new List<Post>();
+            if (search != null)
+            {
+                result = posts.ToList().Where(p => p.Body.ToLower().Contains(search.ToLower())).ToList();
+            }
+            else
+            {
+                result = posts;
+            }
+
+            ViewBag.posts = result;
+            return View(posts.ToList());
         }
 
         // GET: Posts/Details/5
